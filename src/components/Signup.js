de525895e3129b234f6197dcd34f02343c40ddc0
RobertoSuarez <electrosonix12@gmail.com>
2023-08-11 15:22:34 -0500

    Mejoras en el registro de estudiantes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,24 +1,48 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Form, Alert } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
+import { Link as LinkNavigation, useNavigate } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { useUserAuth } from '../context/UserAuthContext';
+import {
+  Button,
+  CircularProgress,
+  IconButton,
+  InputAdornment,
+  Link,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [success, setSuccess] = useState('');
   const { signUp } = useUserAuth();
   let navigate = useNavigate();
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
     try {
-      await signUp(email, password);
-      navigate('/login');
+      const credenciales = await signUp(email, password);
+      console.log(credenciales);
+
+      setSuccess('Usuario registrado correctamente');
+
+      setTimeout(() => {
+        navigate('/login');
+      }, 5000);
     } catch (err) {
-      setError(err.message);
+      if (err.message === 'Firebase: Error (auth/email-already-in-use).') {
+        setError('El correo ya esta en uso');
+      }
       //console.log('Mensaje de depuración');
       //navigate('/signup');
     }
@@ -27,34 +51,54 @@ const Signup = () => {
   return (
     <>
       <div className="p-4 box">
-        <h2 className="mb-3">Registro de estudiantes</h2>
+        <Typography variant="h4" textAlign={'center'} mb={3}>
+          Registro de estudiantes
+        </Typography>
         {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Dirección de correo electrónico"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Contraseña"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Registrarse
-            </Button>
-          </div>
-        </Form>
+        {success && (
+          <Alert variant="success">
+            <Typography variant="subtitle1">{success}</Typography>
+            <Typography variant="caption">
+              Se redireccionara en un segundo <CircularProgress size={16} />
+            </Typography>
+          </Alert>
+        )}
+        <Stack spacing={3}>
+          <TextField
+            label="Correo electronico"
+            value={email}
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            variant="outlined"
+          />
+          <TextField
+            label="Contraseña"
+            variant="outlined"
+            type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleClickShowPassword}>
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Button variant="contained" onClick={handleSubmit}>
+            Registrarse
+          </Button>
+        </Stack>
       </div>
       <div className="p-4 box mt-3 text-center">
-        ¿Ya tienes una cuenta? <Link to="/login">Iniciar sesión</Link>
+        <Typography variant="body1" color="text.secondary">
+          ¿Ya tienes una cuenta?{' '}
+          <Link color="primary" component={LinkNavigation} to="/login">
+            Iniciar sesión
+          </Link>
+        </Typography>
       </div>
     </>
   );
