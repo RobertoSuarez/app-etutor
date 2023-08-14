7b1f4af28e3d31993c21a1e8138dda46c076b121
Karla_Almea <kalmeav@uteq.edu.ec>
2023-08-13 21:00:21 -0500

    Generar PDF y ocultar menú

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,10 +24,14 @@ import {
   Stack,
   Typography,
 } from '@mui/material';
+import { FaBars } from 'react-icons/fa';
 
 export default function Sidebar() {
-  const [sidebar, setSidebar] = useState(false);
-  const ShowSidebar = () => setSidebar(!sidebar);
+  const [menuVisible, setMenuVisible] = useState(true); // Inicialmente visible
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
 
   const { user, logOut } = useUserAuth();
   const navigate = useNavigate();
@@ -83,33 +87,34 @@ export default function Sidebar() {
 
   return (
     <>
-      <div id="menu-btn" className="menu-button" onClick={ShowSidebar}>
-        =
-      </div>
-      <div
-        className={sidebar ? 'sidebar active-sidebar' : 'sidebar'}
-        onClick={ShowSidebar}
-      >
-        <Box
-          sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}
-          mt={3}
-        >
-          <Stack direction={'row'} spacing={2} alignItems={'center'}>
-            <Avatar
-              alt={user.displayName}
-              src={(user && user.photoURL) || ProfileImage}
-              sx={{ width: 48, height: 48 }}
-            />
-            <Stack>
-              <Typography variant="subtitle2">
-                {user && user.displayName}
-              </Typography>
-              <Typography variant="body2">{user && user.email}</Typography>
-            </Stack>
-          </Stack>
+      <div className={`sidebar ${menuVisible ? '' : 'sidebar-contenedor'}`}>
+        <Box sx={{ marginLeft: '10px', marginBottom: '10px' }}>
+          <FaBars onClick={toggleMenu} className="menu-icon" />
         </Box>
-
-        {/* <div className="row">
+        <div className={`sidebar ${menuVisible ? '' : 'sidebar-hidden'}`}>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+            mt={3}
+          >
+            <Stack direction={'row'} spacing={2} alignItems={'center'}>
+              <Avatar
+                alt={user.displayName}
+                src={(user && user.photoURL) || ProfileImage}
+                sx={{ width: 48, height: 48 }}
+              />
+              <Stack>
+                <Typography variant="subtitle2">
+                  {user && user.displayName}
+                </Typography>
+                <Typography variant="body2">{user && user.email}</Typography>
+              </Stack>
+            </Stack>
+          </Box>
+          {/* <div className="row">
           <div className="user-image">
             <img src={(user && user.photoURL) || ProfileImage} />
           </div>
@@ -120,36 +125,33 @@ export default function Sidebar() {
             <li>{CheckUser(user) ? <></> : <LoginButton />}</li>
           </div>
         </div> */}
+          <Divider sx={{ backgroundColor: 'black', marginTop: 3 }} />
+          <List>
+            {Links.map((item, index) => {
+              return (
+                <ListItem disablePadding key={index}>
+                  <ListItemButton
+                    LinkComponent={Link}
+                    to={item.path}
+                    selected={location.pathname === item.path}
+                  >
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.title}></ListItemText>
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
 
-        <Divider sx={{ backgroundColor: 'black', marginTop: 3 }} />
-
-        <List>
-          {Links.map((item, index) => {
-            return (
-              <ListItem disablePadding key={index}>
-                <ListItemButton
-                  LinkComponent={Link}
-                  to={item.path}
-                  selected={location.pathname === item.path}
-                >
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText primary={item.title}></ListItemText>
-                </ListItemButton>
-              </ListItem>
-            );
-          })}
-
-          <ListItem disablePadding>
-            <ListItemButton onClick={handleLogout}>
-              <ListItemIcon>
-                <LogoutOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText primary={'Cerrar Sesión'}></ListItemText>
-            </ListItemButton>
-          </ListItem>
-        </List>
-
-        {/* <ul className="row">
+            <ListItem disablePadding>
+              <ListItemButton onClick={handleLogout}>
+                <ListItemIcon>
+                  <LogoutOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Cerrar Sesión'}></ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </List>
+          {/* <ul className="row">
           {Links.map((item, index) => {
             return (
               <Link to={item.path} key={index} className="sidebar-link">
@@ -164,6 +166,7 @@ export default function Sidebar() {
             <li>{CheckUser(user) ? <LogoutButton /> : <></>}</li>
           </div>
         </ul> */}
+        </div>
       </div>
     </>
   );
