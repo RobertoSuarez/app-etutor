0637eb16c2efdb76c9a0015b1e5534695b79df99
RobertoSuarez <electrosonix12@gmail.com>
2023-08-24 23:57:31 -0500

    estructura inicial del curso

--- src/hooks/useAddCourse.js
+++ src/hooks/useAddCourse.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useUserAuth } from '../context/UserAuthContext';
 import { db } from '../firebase';
-import { addDoc, collection } from 'firebase/firestore';
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
 
 export const useAddCourse = () => {
   const [loading, setLoading] = useState(false);
@@ -10,10 +10,32 @@ export const useAddCourse = () => {
 
   const handleSaveCourse = async (course) => {
     setLoading(true);
-    const docRef = await addDoc(collection(db, 'courses'), {
-      ...course,
+    const { name, category } = course;
+
+    // Definimos la estructura inicial que tendra el curso.
+    const courseStruct = {
       userID: user.uid,
-    });
+      basics: {
+        title: name,
+        subTitle: '',
+        category: category,
+        description: '',
+        date: Timestamp.now(), // fecha de creación
+        videoURL: '',
+        photoURL: '',
+        public: false,
+      },
+      goals: {
+        learningObjectives: ['Define tu primero objetivo para el curso'], // Objetivos de aprendisaje del curso.
+        studentRequirements: ['Requisito para tomar el curso'], // requisitos minimos de estudiantes para tomar este curso.
+      },
+      program: {
+        sections: [], // se define las secciones del curso, los videos y objetivos por clases
+      },
+    };
+
+    // registramos los datos en firestore
+    const docRef = await addDoc(collection(db, 'courses'), courseStruct);
 
     setLoading(false);
     return docRef;
